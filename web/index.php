d2faae5269dd143465734e055147bf06796f408e
Aleksandr Sholk <Aleksandr.Sholk@InToSoft.by>
2019-05-19 19:44:06 +0300

    First commit

---
+++ web/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>InToPeople</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
+    <link href="styles/css/index.css" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <div class="main-wrap">
+            <a class="logo" href="/"></a>
+            <nav class="main-menu">
+                <a href="#">Over Ons</a>
+                <a href="#">Nearshore Development</a>
+                <a href="#">Payrolling</a>
+                <a href="#">Detachering</a>
+                <a href="#">Contact</a>
+            </nav>
+            <a class="lang-nl" href="#"></a>
+            <div class="top-text">
+                <div class="sub-menu">
+                    <a href="#">Schrijf je in</a>
+                    <a href="#">Voor onze e-nieuwsbrief</a>
+                </div>
+            </div>
+            <div class="middle-text">
+                <h1>
+                    projectcoördinatie, consultancy
+                    en begeleiding van nearshore activiteiten
+                </h1>
+            </div>
+            <div class="bottom-text">
+                <p>Lees meer over onze ervaring in volgende richtingen:</p>
+                <div class="sub-menu">
+                    <a href="#"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-gray">
+        <div class="main-wrap">
+            <h1>IntoPeople</h1>
+            <p>
+                provides software solutions for everyone. Our solutions consist of nearshore development,
+                flexible staffing, payrolling and a combination of those services. We offer you security,
+                quality and competitive pricing. We offer easy solutions tailored through operational knowledge,
+                business expertise, flexibility and creativity.
+            </p>
+        </div>
+    </div>
+    <div class="bg-white">
+        <div class="main-wrap">
+            <h1>Clients</h1>
+            <p>
+                We create new awesome interactive digital products. Researching market, a-b testing, branding,
+                mobile & web development, wireframes and interactive prototypes for early stages services.
+                We focused on user experience and mobile applications.
+            </p>
+        </div>
+    </div>
+    <div class="bg-gray">
+        <div class="main-wrap">
+            <h1>Want work with us?</h1>
+            <p class="larger">
+                We are excited to work with services and complicated applications,<br>
+                so if your project one of those you will get a small discount
+            </p>
+            <form action="/" method="post">
+                <div class="form-row">
+                    <div class="form-field half-width">
+                        <input type="text" placeholder="Your name">
+                    </div>
+                    <div class="form-field half-width">
+                        <input type="text" placeholder="Email or phone number">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-field full-width">
+                        <input type="text" placeholder="Comments">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <button type="submit">SEND</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="main-wrap">
+            <div class="logo"></div>
+            <div class="social-links">
+                <a href="#" class="twitter"></a>
+                <a href="#" class="facebook"></a>
+                <a href="#" class="linkedin"></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
