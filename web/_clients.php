1d15117e45f05c0cc76d7f78e4e6147a035872a5
Aleksandr Sholk <Aleksandr.Sholk@InToSoft.by>
2019-05-20 21:08:54 +0300

    In progress

---
+++ web/_clients.php
@@ -0,0 +1,21 @@
+<div class="clients bg-white">
+    <div class="main-wrap">
+        <h1>Clients</h1>
+        <div class="middle-width centered extra-margin-bottom">
+            <p class="align-center">
+                We create new awesome interactive digital products. Researching market, a-b testing, branding,
+                mobile & web development, wireframes and interactive prototypes for early stages services.
+                We focused on user experience and mobile applications.
+            </p>
+        </div>
+        <div class="gallery">
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
+        </div>
+    </div>
+</div>
